b6c98e14f38b5b665274e4a6b26d44bfebe78c0f
enriv <164952583+enriv@users.noreply.github.com>
2026-04-22 12:44:17 -0700

    Clean up Empresas class by removing comments
    Removed commented-out properties from Empresas class.

--- Medsi-Contpaqi/SDK/Empresas.cs
+++ Medsi-Contpaqi/SDK/Empresas.cs
@@ -11,10 +11,6 @@ namespace Medsi_Contpaqi.SDK
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string NombreBaseDatos { get; set; }
-        //public string RutaDatos { get; set; }
-        //public string RutaRespaldo { get; set; }
-        //public DateTime UltimaRestauracion { get; set; }
-        //public string UltimoArchivoRespaldo { get; set; }
-        //public DateTime UltimoRespaldo { get; set; }
+        
     }
 }
